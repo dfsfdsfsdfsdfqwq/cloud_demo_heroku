4c281d58a6e6c424088393a8108cbbeb403cae02
letrungtai632002 <106653459+letrungtai632002@users.noreply.github.com>
2022-10-10 11:08:47 +0700

    change 3

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
-app.use('/admin', loginRouter);
+app.use('/login', loginRouter);
 
 
 // catch 404 and forward to error handler
